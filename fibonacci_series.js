7133db7b7314a89689fb92a826b876f5f8850767
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-01-26 17:58:36 +0530

    recursion

---
+++ fibonacci_series.js
@@ -0,0 +1,41 @@
+function runProgram(input) {
+    //Enter code here
+    input = input.trim().split("\n")
+    var n = +input[0]
+
+    console.log(fibonacciSeries(n))
+}
+
+function fibonacciSeries(n) {
+    // Stop condition
+    if (n == 0)
+        return 0;
+
+    // Stop condition
+    if (n == 1 || n == 2)
+        return 1;
+
+    // Recursion function
+    else
+        return (fibonacciSeries(n - 1) + fibonacciSeries(n - 2));
+}
+if (process.env.USERNAME == "Girraj Goyal") {
+    runProgram(`4`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function(input) {
+        read += input;
+    });
+    process.stdin.on("end", function() {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function() {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
