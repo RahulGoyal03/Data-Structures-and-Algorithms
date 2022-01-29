5164debd2f606e3c08f8094e351b0f9c7834afac
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-01-29 22:11:51 +0530

    dsa

---
+++ Unit 3/MasaiMover2.js
@@ -0,0 +1,50 @@
+function runProgram(input) {
+    input = input.trim().split("\n");
+    let tests = input[0].trim().split(" ").map(Number);
+    let weight = tests[0];
+    let trucks = tests[1];
+    let arr = input[1].trim().split(" ").map(Number);
+    let count = []
+    for (let i = 0; i < trucks; i++) {
+        if (arr[i] % weight) {
+            let freq = 0;
+            while (weight != 0) {
+                weight -= arr[i];
+                freq++;
+            }
+            if (weight == 0) {
+                count.push(freq);
+            }
+        } else {
+            weight -= arr[i];
+            let freq = 0;
+            for (let j = i; j < trucks; j++) {
+                while (weight != 0) {
+                    weight -= arr[j];
+                    freq++;
+                }
+                if (weight == 0) {
+                    count.push(freq);
+                }
+            }
+        }
+    }
+    console.log(count);
+} {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function(input) {
+        read += input;
+    });
+    process.stdin.on("end", function() {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function() {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
