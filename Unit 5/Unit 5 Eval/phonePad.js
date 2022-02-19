966b4063a2510a913fae4e86899bdc5c6f7fcef7
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-02-19 23:16:05 +0530

    dsa

---
+++ Unit 5/Unit 5 Eval/phonePad.js
@@ -0,0 +1,47 @@
+function runProgram(input) {
+   //Enter the code here
+   var str = input
+   var ans = ""
+   phone_pad(ans,str)
+}
+function phone_pad(ans,str){
+    // console.log(str)
+    var text = "a"
+    if(str.length == 0){    
+        console.log(ans);
+       return
+    }
+    let digit = str.charAt(0) - "0"
+    // console.log(str.charAt(0))
+    // console.log(digit)
+    for(var i = (digit - 1) * 3; i < (digit*3); i++){
+        let cha = (text.charCodeAt(0) + i)
+       let ch = String.fromCharCode(cha)
+     phone_pad(ans + ch, str.substring(1))
+    }
+
+   
+     
+}
+
+
+if (process.env.USERNAME === 'DELL') {
+    runProgram(`22`)
+} else {
+    process.stdin.resume()
+    process.stdin.setEncoding('ascii')
+    let read = ''
+    process.stdin.on('data', function(input) {
+        read += input
+    })
+    process.stdin.on('end', function() {
+        read = read.replace(/\n$/, '')
+        read = read.replace(/\n$/, '')
+        runProgram(read)
+    })
+    process.on('SIGINT', function() {
+        read = read.replace(/\n$/, '')
+        runProgram(read)
+        process.exit(0)
+    })
+}
