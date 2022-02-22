8747f5ff1e211789b6c85991a2943fab2625c838
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-02-22 13:25:32 +0530

    abc

---
+++ Unit 6/abc.js
@@ -0,0 +1,17 @@
+// x = 5 && 22
+// y = 2 || 11
+// console.log(x+y)
+
+
+
+// console.log(name)
+name = "rahul"
+
+
+var name
+console.log(name)
+
+// arr=[1,2,3,4,5]
+// console.log(arr.map((a) => a*5))
+
+// console.log([1] === [1])
