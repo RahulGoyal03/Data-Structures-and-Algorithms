0e492a39bd6b88cefe7e78e4bc36a65dbc941075
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-02-22 13:25:53 +0530

    call stack

---
+++ Unit 6/callstack.js
@@ -0,0 +1,30 @@
+// var x = 1;
+// a();
+// console.log(x);
+
+// function a() {
+//   let x = 10;
+//   console.log(x);
+// }
+
+// var x = 20;
+// if(x ==20) {
+// let x = 10;
+// }
+// var a = x;
+// console.log(a);
+
+// let a = 10;
+
+// a = 101;
+// console.log(a);\
+
+
+
+
+// var a = 101
+// let a = 10
+
+
+// console.log(a)
+
